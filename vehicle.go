6a73fe7b67e3f4e161fd2cf3705dd264d479ee8b
Paul Day <pday@ordermygear.com>
2020-05-08 18:31:22 -0500

    gimmie a break

--- vehicle.go
+++ vehicle.go
@@ -56,9 +56,13 @@ const (
 	extraNegative rating = -1.2
 )
 
+var vehicleResult map[string]feedbackResult
+
+var inventory []vehicle
+
 func init() {
 
-	inventory := []vehicle{
+	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
 		bike{"Iron 1200", "Harley"},
 		car{"Sonata", "Hyundai", "Sedan"},
@@ -71,7 +75,7 @@ func init() {
 		truck{"F-150", "Ford", "Truck"},
 		truck{"RAM1500", "Dodge", "Truck"}}
 
-	vehicleResult := make(map[string]feedbackResult)
+	vehicleResult = make(map[string]feedbackResult)
 
 }
 
